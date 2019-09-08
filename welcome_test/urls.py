118a98e9923f218b1894312291bb94786f4954bf
Aditya Sreepad <aditya.g.sreepad@gmail.com>
2019-09-08 20:31:00 +0530

    implemented REST post method

--- welcome_test/urls.py
+++ welcome_test/urls.py
@@ -5,7 +5,12 @@ from .import views
 router = routers.DefaultRouter()
 router.register(r'users', views.UserViewSet)
 router.register(r'groups', views.GroupViewSet)
-
+#router.register(r'login', views.Login,basename='Login')
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
-urlpatterns = router.urls
+
+#urlpatterns = router.urls
+urlpatterns = [
+    path('', include(router.urls)),
+    path('login', views.Login.as_view(), name="logins"),
+]
--- welcome_test/views.py
+++ welcome_test/views.py
@@ -1,7 +1,10 @@
 from django.contrib.auth.models import User, Group
 from rest_framework import viewsets
 from .serializers import UserSerializer, GroupSerializer
-
+from rest_framework.views import APIView
+#from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
 
 class UserViewSet(viewsets.ModelViewSet):
     """
@@ -17,3 +20,23 @@ class GroupViewSet(viewsets.ModelViewSet):
     """
     queryset = Group.objects.all()
     serializer_class = GroupSerializer
+
+#@api_view(['GET', 'POST'])
+#def login(request):
+#    if request.method =='POST':
+#        return Response('Login successful',status=status.HTTP_201_CREATED)
+
+class Login(APIView):
+    def post(self, request, format=None):
+        print(request.headers['Host'])
+        print(request.headers['username'])
+        print(request.headers['password'])
+
+        if request.headers['username'] in ('Aditya','Sushanth'):
+            return Response('Login successful, Welcome :'+request.headers['username'],status=status.HTTP_201_CREATED)
+        else:
+            return Response('Login Failed, Invalid user :'+request.headers['username'], status=status.HTTP_400_BAD_REQUEST)
+
+    def get(self, request, format=None):
+        print(request.headers)
+        return Response('get request not allowed',status=status.HTTP_400_BAD_REQUEST)
